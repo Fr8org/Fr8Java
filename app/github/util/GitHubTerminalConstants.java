64b6059dd2fc99efb4f5479cf31866b4406da36d
cpretzer <charles@fr8.co>
2016-06-19 04:43:30 -0700

    Trying to fix heroku

--- app/github/util/GitHubTerminalConstants.java
+++ app/github/util/GitHubTerminalConstants.java
@@ -6,6 +6,10 @@ import co.fr8.data.interfaces.dto.WebServiceDTO;
 import co.fr8.data.states.ActivityCategoryEnum;
 import co.fr8.data.states.AuthenticationTypeEnum;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static co.fr8.play.ApplicationConstants.LIST_REPOSITORIES_ACTIVITY_NAME;
 import static co.fr8.play.ApplicationConstants.TERMINAL_HOST;
 import static co.fr8.play.ApplicationConstants.TERMINAL_NAME;
@@ -13,7 +17,7 @@ import static co.fr8.play.ApplicationConstants.TERMINAL_NAME;
 /**
  * TODO: DOCUMENT
  */
-public class GithubTerminalConstants {
+public class GitHubTerminalConstants {
 
   public static final TerminalDTO GITHUB_TERMINAL_DTO =
       new TerminalDTO(TERMINAL_NAME, "GitHub", "1", /* TODO: decouple this from play */TERMINAL_HOST,
@@ -27,4 +31,19 @@ public class GithubTerminalConstants {
           "1", GITHUB_WEBSERVICE_DTO, GITHUB_TERMINAL_DTO, ActivityCategoryEnum.MONITORS,
           true, 380);
 
+  public static final ActivityTemplateDTO GITHUB_GET_EXAMPLE_DTO =
+      new ActivityTemplateDTO("Git GET Example", "Get all repositories", "1",
+          GITHUB_WEBSERVICE_DTO, GITHUB_TERMINAL_DTO, ActivityCategoryEnum.RECEIVERS, true, 100);
+
+  public static final ActivityTemplateDTO GITHUB_PROCESS_EXAMPLE_DTO =
+      new ActivityTemplateDTO("Git PROCESS Example", "Process git info", "1",
+          GITHUB_WEBSERVICE_DTO, GITHUB_TERMINAL_DTO, ActivityCategoryEnum.PROCESSORS, true, 380);
+
+  public static final ActivityTemplateDTO GITHUB_FORWARD_EXAMPLE_DTO =
+      new ActivityTemplateDTO("Git FORWARD Example", "Forward git info", "1",
+          GITHUB_WEBSERVICE_DTO, GITHUB_TERMINAL_DTO, ActivityCategoryEnum.FORWARDERS, true, 380);
+
+
+  public static final List<ActivityTemplateDTO> ALL_ACTIVITIES = new ArrayList<>(
+      Arrays.asList(GITHUB_LIST_REPOS_TEMPLATE_DTO, GITHUB_GET_EXAMPLE_DTO, GITHUB_PROCESS_EXAMPLE_DTO, GITHUB_FORWARD_EXAMPLE_DTO));
 }
